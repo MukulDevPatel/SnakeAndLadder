2624144feb6038197c1d515bd6c59f45dcd5cd1a
MukulDevPatel <patelmukuldev@gmail.com>
2023-04-29 18:33:08 +0530

    [Mukul Dev] Add: Game sarts with zero player position

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadderProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Program");
+            SnakeAndLadderPlay snakeAndLadder = new SnakeAndLadderPlay();
+        }
+    }
+}
